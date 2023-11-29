02faad938dfba372d8df398e325973e828cacfce
Macktireh <abdimack97@gmail.com>
2023-11-29 17:49:29 +0100

    added hooks

---
+++ src/hooks/useColorScheme.tsx
@@ -0,0 +1,5 @@
+import { ColorSchemeName, useColorScheme as _useColorScheme, } from "react-native";
+
+export const useColorScheme = (): NonNullable<ColorSchemeName> => {
+  return _useColorScheme() as NonNullable<ColorSchemeName>;
+};
